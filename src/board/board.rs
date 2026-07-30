ce5ab2c4489eacd357fad51de7e7a2607b72da73
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:45:17 +0100

    feat(board): add is_valid collision check

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,4 +1,5 @@
 use crate::board::cell::Cell;
+use crate::piece::piece::Piece;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Board {
@@ -10,5 +11,25 @@ impl Board {
         Self {
             cells: [[Cell::Empty; 10]; 20],
         }
+    }
+
+    pub fn is_valid(&self, piece: &Piece) -> bool {
+        for row in 0..4 {
+            for col in 0..4 {
+                if !piece.shape.cells[row][col] {
+                    continue;
+                }
+                let x = piece.position.x + col as i32;
+                let y = piece.position.y + row as i32;
+
+                if x < 0 || x >= 10 || y < 0 || y >= 20 {
+                    return false;
+                }
+                if let Cell::Filled(_) = self.cells[y as usize][x as usize] {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
