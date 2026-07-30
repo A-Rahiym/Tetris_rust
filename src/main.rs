3e13f4feee91ccbe720f2c29c4bc15504712930d
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:05:41 +0100

    feat(main): wire game loop to Renderer::draw

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,18 @@ mod renderer;
 
 use macroquad::prelude::*;
 
+use crate::game::game::Game;
+use crate::renderer::renderer::Renderer;
 
 #[macroquad::main("Tetris")]
 async fn main() {
-    loop {
-        clear_background(RED);
-        draw_line(40.0, 0.0, 40.0, 40.0, 15.0, BLUE);
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+    let mut game = Game::new();
 
-        if is_key_pressed(KeyCode::Up) {
-            println!("key pressed")
-        }
+    loop {
+        clear_background(BLACK);
+
+        Renderer::draw(&game);
 
         next_frame().await
     }
-
-   
 }
