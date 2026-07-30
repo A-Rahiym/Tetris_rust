404dee5b4c844e1f700e75ffe7346be18bfa5509
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:45:17 +0100

    feat(input): add keyboard poll mapping to PlayerAction

--- src/input/input.rs
+++ src/input/input.rs
@@ -1,36 +1,24 @@
+use crate::input::actions::PlayerAction;
 use macroquad::prelude::*;
 
-use crate::input::actions::PlayerAction;
-
-pub struct Input;
-
-impl Input {
-    pub fn poll() -> Option<PlayerAction> {
-
-        if is_key_pressed(KeyCode::Left) {
-            return Some(PlayerAction::MoveLeft);
-        }
-
-        if is_key_pressed(KeyCode::Right) {
-            return Some(PlayerAction::MoveRight);
-        }
-
-        if is_key_pressed(KeyCode::Up) {
-            return Some(PlayerAction::Rotate);
-        }
-
-        if is_key_pressed(KeyCode::Down) {
-            return Some(PlayerAction::SoftDrop);
-        }
-
-        if is_key_pressed(KeyCode::Space) {
-            return Some(PlayerAction::HardDrop);
-        }
-
-        if is_key_pressed(KeyCode::Escape) {
-            return Some(PlayerAction::Pause);
-        }
-
-        None
+pub fn poll() -> Option<PlayerAction> {
+    if is_key_pressed(KeyCode::Left) {
+        return Some(PlayerAction::MoveLeft);
     }
+    if is_key_pressed(KeyCode::Right) {
+        return Some(PlayerAction::MoveRight);
+    }
+    if is_key_pressed(KeyCode::Up) {
+        return Some(PlayerAction::Rotate);
+    }
+    if is_key_pressed(KeyCode::Down) {
+        return Some(PlayerAction::SoftDrop);
+    }
+    if is_key_pressed(KeyCode::Space) {
+        return Some(PlayerAction::HardDrop);
+    }
+    if is_key_pressed(KeyCode::P) {
+        return Some(PlayerAction::Pause);
+    }
+    None
 }
