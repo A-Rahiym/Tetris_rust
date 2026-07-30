ed555ad8beedc369da76db09d9d1000d4f636087
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:03:20 +0100

    fix(board): remove stray Renderer struct and unused Game import

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,7 +1,12 @@
 
 use crate::board::cell::Cell;
+use crate::game::game::Game;
+
+
+pub struct Renderer;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Board{
     pub cells: [[Cell; 10]; 20],
 }
+
