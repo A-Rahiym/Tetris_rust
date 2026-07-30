f47b36b56381f05f903b2fe8df3f7c7270dad415
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:05:41 +0100

    fix(board): add Board::new and remove stray Renderer struct

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,12 +1,14 @@
-
 use crate::board::cell::Cell;
-use crate::game::game::Game;
-
-
-pub struct Renderer;
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Board{
+pub struct Board {
     pub cells: [[Cell; 10]; 20],
 }
 
+impl Board {
+    pub fn new() -> Self {
+        Self {
+            cells: [[Cell::Empty; 10]; 20],
+        }
+    }
+}
