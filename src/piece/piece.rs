0eae60c5404847fee4b26854b94e8b2acd50414a
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-30 16:03:20 +0100

    feat(piece): add Piece rotation methods and make Rotation Copy

--- src/piece/piece.rs
+++ src/piece/piece.rs
@@ -1,10 +1,49 @@
 use crate::piece::kind::PieceKind;
 use crate::piece::position::Position;
 use crate::piece::rotation::Rotation;
+use crate::piece::shape::{Shape, get_shape};
 
 #[ derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Piece {
     pub kind: PieceKind,
     pub position: Position,
     pub rotation: Rotation,
+    pub shape: Shape,
+}
+
+impl Piece {
+    pub fn new(
+        kind: PieceKind,
+        position: Position,
+        rotation: Rotation,
+    ) -> Self {
+        let shape = get_shape(kind, rotation);
+
+        Self {
+            kind,
+            shape,
+            position,
+            rotation,
+        }
+    }
+
+    pub fn rotate_clockwise(&mut self) {
+        self.rotation = match self.rotation {
+            Rotation::North => Rotation::East,
+            Rotation::East => Rotation::South,
+            Rotation::South => Rotation::West,
+            Rotation::West => Rotation::North,
+        };
+        self.shape = get_shape(self.kind, self.rotation);
+    }
+
+    pub fn rotate_counter_clockwise(&mut self) {
+        self.rotation = match self.rotation {
+            Rotation::North => Rotation::West,
+            Rotation::West => Rotation::South,
+            Rotation::South => Rotation::East,
+            Rotation::East => Rotation::North,
+        };
+        self.shape = get_shape(self.kind, self.rotation);
+    }
 }
--- src/piece/rotation.rs
+++ src/piece/rotation.rs
@@ -1,6 +1,6 @@
 
 #[allow(dead_code)]
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Rotation {
     North,
     East,
