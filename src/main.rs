836e1af57dd6b24afc5d5c5a2d00a2c1a9b50caa
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-08-01 22:22:03 +0100

    feat(main): enlarge window and draw game over screen

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,31 @@ use macroquad::prelude::*;
 use crate::game::game::Game;
 use crate::renderer::renderer::Renderer;
 
-#[macroquad::main("Tetris")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Tetris".to_owned(),
+        window_width: 900,
+        window_height: 720,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let mut game = Game::new();
+
     loop {
         clear_background(BLACK);
-        println!("New frame");
-        println!("Game 1: {:?}", game);
+
         let action = input::input::poll();
         game.update(action);
 
         Renderer::draw(&game);
+
+        if game.state == game::state::GameState::GameOver {
+            draw_text("GAME OVER", 300.0, 360.0, 50.0, RED);
+        }
+
         next_frame().await
     }
 }
